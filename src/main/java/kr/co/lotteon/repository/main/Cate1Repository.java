e0e3281f26ed534e7267a35bc58311bf0d854b93
Dong Han <lux00leo@gmail.com>
2023-10-15 04:01:45 +0900

    cate1Repository 수정

--- src/main/java/kr/co/lotteon/repository/main/Cate1Repository.java
+++ src/main/java/kr/co/lotteon/repository/main/Cate1Repository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Cate1Repository extends JpaRepository<ProductCate1Entity, String> {
+public interface Cate1Repository extends JpaRepository<ProductCate1Entity, Integer> {
 }
