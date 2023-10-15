3ef40a8649ed9592e6ff3833c5b6c51d6ec7152f
Dong Han <lux00leo@gmail.com>
2023-10-15 22:46:17 +0900

    admin controller package

---
+++ src/main/java/kr/co/lotteon/controller/admin/cs/AdminCsController.java
@@ -0,0 +1,13 @@
+package kr.co.lotteon.controller.admin.cs;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/cs")
+public class AdminCsController {
+}
